943cea640ea4366104ec1b3323fca5b189885660
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-07-02 09:26:50 +0100

    deploy

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.post("/uir-chat-bot", async (req: Request, res: Response) => {
   // Get The 5 latest message for understand the context
 
   const latestMessages = await prisma.message.findMany({
-    take: 2,
+    take: 5,
     where: {
       from: message.From,
     },
